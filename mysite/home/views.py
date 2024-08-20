f42981140833e200165cf601d501705c41ae015d
acsolle66 <csolle.coding@gmail.com>
2024-08-20 11:20:04 +0200

    Initial commit

---
+++ mysite/home/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.template.response import TemplateResponse
+
+# Create your views here.
+def home_view(request):
+    return TemplateResponse(request, "home/home.html", {})
