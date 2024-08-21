8faefe1f8b37d8acee5dc9800d6799c1e4f1aec3
acsolle66 <csolle.coding@gmail.com>
2024-08-21 12:31:48 +0200

    add model to admin

--- mysite/ads/models.py
+++ mysite/ads/models.py
@@ -6,14 +6,14 @@ from django.core.validators import MinLengthValidator
 AUTH_USER_MODEL = get_user_model()
 
 
-class Ad(models.Mode):
+class Ad(models.Model):
     title = models.CharField(
         max_length=200,
         validators=[MinLengthValidator(2, "Title must be greater than 2 characters")],
     )
     price = models.DecimalField(max_digits=7, decimal_places=2)
     text = models.TextField()
-    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE())
+    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+
+    # Mysite apps
+    'home',
+    'ads'
 ]
 
 MIDDLEWARE = [
@@ -111,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Bratislava'
 
 USE_I18N = True
 
