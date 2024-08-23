4c01405af9d693bb11a498f3525ca49526b599a8
acsolle66 <csolle.coding@gmail.com>
2024-08-23 16:08:07 +0200

    add login/logout functionality

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 
+
 # Create your views here.
 def home_view(request):
     return TemplateResponse(request, "home/home.html", {})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,8 +43,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Mysite apps
-    "home",
-    "ads",
+    "home.apps.HomeConfig",
+    "ads.apps.AdsConfig",
+    # Third party apps
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGOUT_REDIRECT_URL = "ads:ads_list"
